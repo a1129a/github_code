1f8d56616a8fc3a6c7891022d44c76e9de6f9765
shuaishuai01 <14243990+shuishuai01@user.noreply.gitee.com>
2024-05-27 15:20:56 +0800

    完善代码还差一个下表导航

---
+++ github-code1/test1/test03/js/script.js
@@ -0,0 +1,30 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
+
+// 为左右箭头添加点击事件监听器
+document.querySelector(".prev").addEventListener("click", function() {
+    plusSlides(-1);
+});
+
+document.querySelector(".next").addEventListener("click", function() {
+    plusSlides(1);
+});
